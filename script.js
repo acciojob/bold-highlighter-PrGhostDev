44166605042ec1fc0716d3efa3cf5153f2c5effb
PrGhostDev <139947007+PrGhostDev@users.noreply.github.com>
2023-10-02 18:41:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
-
+    const strongElements = document.querySelectorAll('p strong'); 
+    strongElements.forEach((element) => {
+        element.style.color = 'green';
+    });
 }
 
 
 function return_normal() {
     //Write your code here
-
+    const strongElements = document.querySelectorAll('p strong');
+      
+    strongElements.forEach((element) => {
+        element.style.color = 'black';
+    });
     
 }
